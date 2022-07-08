0c36a3fcbb020e164621ed5707f5e4ad373099ed
aleksastojsic <stojsic.aleksa@gmail.com>
2022-07-08 19:50:04 +0200

    Fix: finally deploy and hpefuly removed indexing

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,30 +17,31 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    // `gatsby-plugin-sitemap`,
-    // `gatsby-plugin-robots-txt`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-robots-txt`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`,
     {
-      // resolve: `gatsby-plugin-google-gtag`,
-      // options: {
-      //   // You can add multiple tracking ids and a pageview event will be fired for all of them.
-      //   trackingIds: [
-      //     "G-HM35T1K4D1", // Google Analytics / GA
-      //   ],
-      //   // This object gets passed directly to the gtag config command
-      //   // This config will be shared across all trackingIds
-      //   gtagConfig: {
-      //     anonymize_ip: true,
-      //   },
-      //   pluginConfig: {
-      //     // Puts tracking script in the head instead of the body
-      //     head: false,
-      //   },
-      // },
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-HM35T1K4D1", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+        },
+
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+        },
+      },
     },
     {
       resolve: `gatsby-plugin-manifest`,
